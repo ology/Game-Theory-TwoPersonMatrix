0f8634a5c91004eef545bb2405fba0eb11b6dcec
ology <gene.boggs@gmail.com>
2014-01-28 19:34:29 -0500

    Added basic docs and method stubs

--- lib/Game/Theory/TwoPersonMatrix.pm
+++ lib/Game/Theory/TwoPersonMatrix.pm
@@ -14,11 +14,11 @@ Game::Theory::TwoPersonMatrix - Reduce and analyze two person matrix games
 =head1 SYNOPSIS
 
   use Game::Theory::TwoPersonMatrix;
-  $x = Game::Theory::TwoPersonMatrix->new(%arguments);
+  my $g = Game::Theory::TwoPersonMatrix->new(%arguments);
 
 =head1 DESCRIPTION
 
-A C<Game::Theory::TwoPersonMatrix> ...Why?
+A C<Game::Theory::TwoPersonMatrix> reduces and analyzes a two person matrix game.
 
 =cut
 
@@ -26,32 +26,51 @@ A C<Game::Theory::TwoPersonMatrix> ...Why?
 
 =head2 new()
 
-  $x = Game::Theory::TwoPersonMatrix->new(%arguments);
+  my $g = Game::Theory::TwoPersonMatrix->new(%arguments);
 
 Create a new C<Game::Theory::TwoPersonMatrix> object.
 
-Arguments and defaults:
-  foo => bar,
-  abc => 123,
+Argument defaults:
+  p1 = [[1,0],[0,1]]
+  p2 = [[1,0],[0,1]]
+
+Players are given by a 2D matrix of utilities (or payoffs) such that,
+
+ [ [ u1, u2 .. un] .. [ v1, v2 .. vn ] ] 
+
+Where each B<u>C<i> is a utility or payoff for the strategy B<U>.
 
 =cut
 
 sub new {
     my $class = shift;
     my %args = @_;
-    # Explicit constructor:
     my $self = {
-        foo => $args{foo} || undef,
-        %args # Final override.
+        p1 => $args{p1} || [[1,0],[0,1]],
+        p2 => $args{p2} || [[1,0],[0,1]],
     };
     bless $self, $class;
-    $self->_init(%args);
     return $self;
 }
-sub _init {
-    my ($self, %args) = @_;
-    $self->{foo} ||= 'bar';
-    $self->{abc} ||= 123;
+
+=head2 reduce()
+
+Reduce the game by elimination of strictly dominated strategies.
+
+=cut
+
+sub reduce {
+    my $self = shift;
+}
+
+=head2 nash()
+
+Find the Nash equilibria.
+
+=cut
+
+sub nash {
+    my $self = shift;
 }
 
 1;
