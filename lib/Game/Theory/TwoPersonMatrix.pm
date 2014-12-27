f8d1ee06ab0f773112face791e4a4ac5953f4061
Gene Boggs <gene.boggs@gmail.com>
2014-12-27 00:59:15 -0500

    inc'd ver

--- lib/Game/Theory/TwoPersonMatrix.pm
+++ lib/Game/Theory/TwoPersonMatrix.pm
@@ -12,7 +12,7 @@ use List::MoreUtils qw( all zip );
 use Array::Transpose;
 use List::Util::WeightedChoice qw( choose_weighted );
 
-our $VERSION = '0.20';
+our $VERSION = '0.2001';
 
 =head1 SYNOPSIS
 
