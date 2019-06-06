a0d5d77210a6aad64cd0f8bf93e4eda3827fc3be
Gene Boggs <gene@ology.net>
2019-06-05 17:42:39 -0700

    Increment version

--- lib/Game/Theory/TwoPersonMatrix.pm
+++ lib/Game/Theory/TwoPersonMatrix.pm
@@ -5,7 +5,7 @@ package Game::Theory::TwoPersonMatrix;
 use strict;
 use warnings;
 
-our $VERSION = '0.2200';
+our $VERSION = '0.2201';
 
 use Carp;
 use Algorithm::Combinatorics qw( permutations );
