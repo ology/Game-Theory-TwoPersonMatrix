56673d303e4007787ea002a2b544735e5917d0d7
Gene Boggs <gene@ology.net>
2020-03-29 14:18:50 -0700

    Move comment

--- lib/Game/Theory/TwoPersonMatrix.pm
+++ lib/Game/Theory/TwoPersonMatrix.pm
@@ -178,13 +178,13 @@ sub expected_payoff
         # For each strategy of player 2...
         for my $j ( sort keys %{ $self->{2} } )
         {
+            # Expected value is the sum of the probabilities of each payoff
             if ( $self->{payoff1} && $self->{payoff2} )
             {
                 $expected_payoff->[0] += $self->{1}{$i} * $self->{2}{$j} * $self->{payoff1}[$i - 1][$j - 1];
                 $expected_payoff->[1] += $self->{1}{$i} * $self->{2}{$j} * $self->{payoff2}[$i - 1][$j - 1];
             }
             else {
-                # Expected value is the sum of the probabilities of each payoff
                 $expected_payoff += $self->{1}{$i} * $self->{2}{$j} * $self->{payoff}[$i - 1][$j - 1];
             }
         }
@@ -222,13 +222,13 @@ sub s_expected_payoff
         # For each strategy of player 2...
         for my $j ( sort keys %{ $self->{2} } )
         {
+            # Expected value is the sum of the probabilities of each payoff
             if ( $self->{payoff1} && $self->{payoff2} )
             {
                 $expected_payoff->[0] .= " + $self->{1}{$i} * $self->{2}{$j} * $self->{payoff1}[$i - 1][$j - 1]";
                 $expected_payoff->[1] .= " + $self->{1}{$i} * $self->{2}{$j} * $self->{payoff2}[$i - 1][$j - 1]";
             }
             else {
-                # Expected value is the sum of the probabilities of each payoff
                 $expected_payoff .= " + $self->{1}{$i} * $self->{2}{$j} * $self->{payoff}[$i - 1][$j - 1]";
             }
         }
